3cecba30de16b8f4522a7a5f9df73bcccd291e1f
luwenprangko <louienfrancoaxalan@gmail.com>
2025-02-08 11:06:07 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js")
+  navigator.serviceWorker.register("./sw.js")  // <-- Updated path
     .then(() => console.log("Service Worker Registered"))
     .catch((err) => console.log("Service Worker Failed", err));
 }
